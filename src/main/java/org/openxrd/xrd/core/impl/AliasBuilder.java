806c1fb49cec9550aab684440bd3df46c0ca7c84
Will Norris <will@willnorris.com>
2009-08-17 19:00:04 +0000

    [java-openxrd @ 131b4cea0d8ff6855b6caf4bf03a3c663de4e678] add builder and marshaller stubs for XRD elements
    Original author: Will Norris <will@willnorris.com>
    Date: 2009-06-15 23:07:11-07:00
    
    git-svn-id: file:///Users/willnorris/Projects/svn-import/java-openxrd/trunk@8 3e0f74b0-2db0-4999-8717-f868e1b9a637

---
+++ src/main/java/org/openxrd/xrd/core/impl/AliasBuilder.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2009 University Corporation for Advanced Internet Development, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openxrd.xrd.core.impl;
+
+import org.openxrd.common.xml.XRDConstants;
+import org.openxrd.xrd.common.impl.AbstractXRDObjectBuilder;
+import org.openxrd.xrd.core.Alias;
+
+/**
+ * Builder for {@link AliasImpl} objects.
+ */
+public class AliasBuilder extends AbstractXRDObjectBuilder<Alias> {
+
+    /** Constructor. */
+    public AliasBuilder() {
+    }
+
+    /** {@inheritDoc} */
+    public Alias buildObject() {
+        return buildObject(XRDConstants.XRD_NS, Alias.DEFAULT_ELEMENT_LOCAL_NAME, XRDConstants.XRD_PREFIX);
+    }
+
+    /** {@inheritDoc} */
+    public Alias buildObject(String namespaceURI, String localName, String namespacePrefix) {
+        return new AliasImpl(namespaceURI, localName, namespacePrefix);
+    }
+
+}
